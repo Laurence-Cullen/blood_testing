5fdc73d9b0af5788fda1fdb068683b4311eef906
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-11-03 18:03:54 +0000

    cleaning up and adding attempt at uploaded image decoding

--- process.py
+++ process.py
@@ -5,15 +5,15 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from skimage import io
 from skimage.filters import threshold_otsu
+# import io
 
-
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'projectId': 'ashra-blood',
-})
-
-db = firestore.client()
+# # Use the application default credentials
+# cred = credentials.ApplicationDefault()
+# firebase_admin.initialize_app(cred, {
+#   'projectId': 'ashra-blood',
+# })
+#
+# db = firestore.client()
 
 # map from blood image red band pixel values to a
 # haemoglobin concentration in grams per deci litre
@@ -59,17 +59,23 @@ def main():
     print(conc, 'grams per dL')
 
 
-def handle_test(request):
-    image = request.files.get('fileUpload', '')
-    name = request.form['studentName']
-
-    doc_ref = db.collection(u'children').add({
-        u'name': name,
-        u'haemoglobin_g_per_dL': 4,
-        u'school': u'St. Xavier’s Collegiate School'
-    })
-
-    return 'student is named: ' + name + ', got image:' + str(type(image))
+# def handle_test(request):
+#     image = request.files.get('fileUpload', '')
+#     name = request.form['studentName']
+#
+#     in_memory_file = io.BytesIO()
+#     image.save(in_memory_file)
+#     raw_image_data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+#
+#     haem_conc = image_to_haemoglobin_grams_per_dl(raw_image_data)
+#
+#     doc_ref = db.collection(u'children').add({
+#         u'name': name,
+#         u'haemoglobin_g_per_dL': haem_conc,
+#         u'school': u'St. Xavier’s Collegiate School'
+#     })
+#
+#     return 'student is named: ' + name + ', got image:' + str(type(image))
 
 
 if __name__ == '__main__':
