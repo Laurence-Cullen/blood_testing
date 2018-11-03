3922cd3f486775d6086e759d33f227bf245302a5
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-11-03 17:13:19 +0000

    adding web requests

--- process.py
+++ process.py
@@ -1,9 +1,19 @@
+import firebase_admin
 import numpy as np
 import numpy.ma as ma
+from firebase_admin import credentials
+from firebase_admin import firestore
 from skimage import io
-
 from skimage.filters import threshold_otsu
 
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'ashra-blood',
+})
+
+db = firestore.client()
 
 # map from blood image red band pixel values to a
 # haemoglobin concentration in grams per deci litre
@@ -47,6 +57,19 @@ def main():
 
     conc = image_to_haemoglobin_grams_per_dl(image)
     print(conc, 'grams per dL')
+
+
+def handle_test(request):
+    image = request.files.get('fileUpload', '')
+    name = request.form['studentName']
+
+    doc_ref = db.collection(u'children').add({
+        u'name': name,
+        u'haemoglobin_g_per_dL': 4,
+        u'school': u'St. Xavier’s Collegiate School'
+    })
+
+    return 'student is named: ' + name + ', got image:' + str(type(image))
 
 
 if __name__ == '__main__':
